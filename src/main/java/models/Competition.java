7339995f6168a4be322bf5154ac9ec5c3440a2ec
Ben Robinson <mentionbenjamin@gmail.com>
2018-07-13 13:27:36 +0100

    set up constructor for competition

--- src/main/java/models/Competition.java
+++ src/main/java/models/Competition.java
@@ -1,4 +1,19 @@
 package models;
 
 public class Competition {
+
+    private String title;
+    private Team team;
+
+    public Competition(String title){
+        this.title = title;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+
 }
