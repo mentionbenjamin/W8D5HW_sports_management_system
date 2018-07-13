3dd70b3043e88fd1057f8e9d36f20d0c8e19f52b
Ben Robinson <mentionbenjamin@gmail.com>
2018-07-13 13:17:17 +0100

    set up constructor for team

--- src/main/java/models/Team.java
+++ src/main/java/models/Team.java
@@ -10,4 +10,20 @@ public abstract class Team {
         this.salary = salary;
     }
 
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+
+    public double getSalary(){
+        return this.salary;
+    }
+    public void setSalary(double salary){
+        this.salary = salary;
+    }
+
+
 }
