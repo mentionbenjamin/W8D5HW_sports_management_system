2c2c0cc2176d939c5f51f5f51f4ab0a1b41d261e
Ben Robinson <mentionbenjamin@gmail.com>
2018-07-13 13:23:18 +0100

    set up constructor for player

--- src/main/java/models/Manager.java
+++ src/main/java/models/Manager.java
@@ -1,4 +1,19 @@
 package models;
 
-public class Manager {
+public class Manager extends Team {
+
+    private double budget;
+
+    public Manager(String name, double salary, double budget){
+        super(name, salary);
+        this.budget = budget;
+    }
+
+    public double getBudget(){
+        return this.budget;
+    }
+    public void setBudget(double budget){
+        this.budget = budget;
+    }
+
 }
--- src/main/java/models/Player.java
+++ src/main/java/models/Player.java
@@ -1,4 +1,20 @@
 package models;
 
-public class Player {
+public class Player extends Team {
+
+    private Manager manager;
+
+    public Player(String name, double salary, Manager manager){
+        super(name, salary);
+        this.manager = manager;
+    }
+
+    private Manager getManager(){
+        return this.manager;
+    }
+    private void setManager(Manager manager){
+        this.manager = manager;
+    }
+
+
 }
