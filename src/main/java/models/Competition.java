1334ddbb5d75db257e9a17d48ce006a8f1fb64e8
Ben Robinson <mentionbenjamin@gmail.com>
2018-07-13 13:43:50 +0100

    added annotations for competition class

--- src/main/java/models/Competition.java
+++ src/main/java/models/Competition.java
@@ -1,14 +1,37 @@
 package models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "competitions")
 public class Competition {
 
+    private int id;
     private String title;
     private Team team;
 
-    public Competition(String title){
+    public Competition(int id, String title){
+        this.id = id;
         this.title = title;
     }
 
+    public Competition(){}
+
+
+    @Id
+    @Column(name = "id")
+    public int getId(){
+        return this.id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+
+
+    @Column(name = "title")
     public String getTitle(){
         return this.title;
     }
