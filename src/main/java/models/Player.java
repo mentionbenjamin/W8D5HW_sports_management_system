8414e3db745ffb120c96440f7909d8981514c186
Ben Robinson <mentionbenjamin@gmail.com>
2018-07-15 10:50:16 +0100

    added position-type enum

--- src/main/java/models/Player.java
+++ src/main/java/models/Player.java
@@ -6,17 +6,29 @@ import javax.persistence.*;
 @Table(name = "players")
 public class Player extends Employee {
 
+    private Position position;
     private Manager manager;
     private Team team;
 
 
-    public Player(String name, double salary, Manager manager, Team team) {
+    public Player(String name, double salary, Position position, Manager manager, Team team) {
         super(name, salary);
+        this.position = position;
         this.manager = manager;
         this.team = team;
     }
 
     public Player(){}
+
+
+
+    @Column(name = "position")
+    public Position getPosition(){
+        return this.position;
+    }
+    public void setPosition(Position position){
+        this.position = position;
+    }
 
 
 
---
+++ src/main/java/models/PositionType.java
@@ -0,0 +1,16 @@
+package models;
+
+public enum PositionType {
+
+    GOALKEEPER,
+    LEFTBACK,
+    CENTREBACK,
+    RIGHTBACK,
+    LEFTMIDFIELD,
+    DEFENDINGMIDFIELD,
+    CENTREMIDFIELD,
+    ATTACKINGMIDFIELD,
+    RIGHTMIDFIELD,
+    STRIKER
+
+}
