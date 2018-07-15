d74d2f2286711e2f042eafe2bbfd0ac903f595f2
Ben Robinson <mentionbenjamin@gmail.com>
2018-07-15 20:48:40 +0100

    tidied runner file

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -8,17 +8,19 @@ public class Runner {
 
     public static void main(String [] args){
 
-
+        // TEAMS
         Team codeClan = new Team("Code Clan", 10);
         DBHelper.save(codeClan);
         Team northCoders = new Team("North Coders", 15);
         DBHelper.save(northCoders);
 
+        // MANAGERS
         Manager carter = new Manager("Coach Carter", 125000.00, codeClan);
         DBHelper.save(carter);
         Manager kloppo = new Manager("Coach Kloppo", 150000.00, northCoders);
         DBHelper.save(kloppo);
 
+        // PLAYERS
         Player player1 = new Player("Jeremy", 50000.00, 24, PositionType.STRIKER, carter, codeClan, 8.6);
         DBHelper.save(player1);
         Player player2 = new Player("Joe", 60000.00, 21, PositionType.ATTACKINGMIDFIELD, carter, codeClan, 6.5);
@@ -28,6 +30,7 @@ public class Runner {
         Player player4 = new Player("Jamie", 75000.00, 32, PositionType.RIGHTBACK, kloppo, northCoders, 6.2);
         DBHelper.save(player4);
 
+        // COMPETITIONS
         Competition championsLeague = new Competition("Champions League");
         DBHelper.save(championsLeague);
         Competition europaLeague = new Competition("Europa League");
